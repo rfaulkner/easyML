9ca24e627ab138d266472aa411f2875ebb122822
rfaulk <rfaulk@yahoo-inc.com>
2013-11-19 23:06:32 -0800

    add - additional packages including hadoop deps.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
+#   Apache Packages
+#   ---------------
+
 apt-get update
 apt-get install -y apache2
 rm -rf /var/www
 ln -fs /vagrant /var/www
+
+
+#   Geeneral Packages
+#   ---------------
+
 apt-get install git
-apt-get install pip
+apt-get install python-pip
+apt-get install vim
+
+
+#   Hadoop Packages
+#   ---------------
+
+apt-get install software-properties-common
+apt-get install python-software-properties
+
+add-apt-repository ppa:webupd8team/java
+apt-get update && sudo apt-get upgrade
+apt-get install oracle-java7-installer
+
+# Setup hadoop user
+sudo addgroup hadoop
+sudo adduser --ingroup hadoop hduser
+
+sudo add-apt-repository ppa:hadoop-ubuntu/stable
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install hadoop
