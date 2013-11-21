5aec4984446f8d310d36a11d72ef52950a9f590a
rfaulk <rfaulk@yahoo-inc.com>
2013-11-20 23:59:27 -0800

    add - class to support learning RBM layers of a deep network.

--- versus/src/learn.py
+++ versus/src/learn.py
@@ -3,7 +3,50 @@ Module for learning source.
 """
 
 import theano.tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
 import numpy
+
 
 def logistic(x):
     return 1 / (1 + T.exp(-x))
+
+
+class DBN(object):
+
+    def __init__(self, numpy_rng, theano_rng=None, n_ins=784,
+                hidden_layers_sizes=(500, 500), n_outs=10):
+        """This class is made to support a variable number of layers.
+
+        :type numpy_rng: numpy.random.RandomState
+        :param numpy_rng: numpy random number generator used to draw initial
+                weights
+
+        :type theano_rng: theano.tensor.shared_randomstreams.RandomStreams
+        :param theano_rng: Theano random generator; if None is given one is
+                       generated based on a seed drawn from `rng`
+
+        :type n_ins: int
+        :param n_ins: dimension of the input to the DBN
+
+        :type n_layers_sizes: list of ints
+        :param n_layers_sizes: intermediate layers size, must contain
+                           at least one value
+
+        :type n_outs: int
+        :param n_outs: dimension of the output of the network
+        """
+
+        self.sigmoid_layers = []
+        self.rbm_layers = []
+        self.params = []
+        self.n_layers = len(hidden_layers_sizes)
+
+        assert self.n_layers > 0
+
+        if not theano_rng:
+            theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
+
+        # allocate symbolic variables for the data
+        self.x = T.matrix('x')  # the data is presented as rasterized images
+        self.y = T.ivector('y')  # the labels are presented as 1D vector of
+                                 # [int] labels
