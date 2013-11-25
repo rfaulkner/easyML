e73e800ad8b056cf4fb54a7fa4a9ab48ed267129
rfaulk <rfaulk@yahoo-inc.com>
2013-11-24 18:19:23 -0800

    add - class to store hidden layers of a DBN.

--- versus/src/learn.py
+++ versus/src/learn.py
@@ -2,13 +2,57 @@
 Module for learning source.
 """
 
+import theano.config as config
 import theano.tensor as T
+import theano.shared as shared
 from theano.tensor.shared_randomstreams import RandomStreams
 import numpy
 
 
-def logistic(x):
-    return 1 / (1 + T.exp(-x))
+class HiddenLayer(object):
+    """
+    Represents the hidden layer of a DBN
+    """
+    
+    def __init__(self, rng, _input, n_in, n_out, activation=T.tanh):
+        """
+        Typical hidden layer of a MLP: units are fully-connected and have
+        sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
+        and the bias vector b is of shape (n_out,).
+
+        NOTE : The nonlinearity used here is tanh
+
+        Hidden unit activation is given by: tanh(dot(input,W) + b)
+
+        :type rng: numpy.random.RandomState
+        :param rng: a random number generator used to initialize weights
+
+        :type input: theano.tensor.dmatrix
+        :param input: a symbolic tensor of shape (n_examples, n_in)
+
+        :type n_in: int
+        :param n_in: dimensionality of input
+
+        :type n_out: int
+        :param n_out: number of hidden units
+
+        :type activation: theano.Op or function
+        :param activation: Non linearity to be applied in the hidden
+                              layer
+        """
+        self.input = _input
+
+        W_values = numpy.asarray(rng.uniform(
+                low=-numpy.sqrt(6. / (n_in + n_out)),
+                high=numpy.sqrt(6. / (n_in + n_out)),
+                size=(n_in, n_out)), dtype=config.floatX)
+        if activation == T.nnet.sigmoid:
+            W_values *= 4
+
+        self.W = shared(value=W_values, name='W')
+
+        b_values = numpy.zeros((n_out,), dtype=config.floatX)
+        self.b = shared(value=b_values, name='b')
 
 
 class DBN(object):
