8189682cea9e4a7f84a8cc4208b66c3e1bb8976b
rfaulk <rfaulk@yahoo-inc.com>
2013-12-01 23:59:00 -0800

    add - modules for data and model IO.

---
+++ versus/tools/dataIO.py
@@ -0,0 +1,11 @@
+"""
+Class family for Data IO classes to handle data ingestion and fetch events
+"""
+
+class DataIO(object):
+
+    def __init__(self):
+        pass
+
+    def write(self):
+        raise NotImplementedError()
---
+++ versus/tools/modelIO.py
@@ -0,0 +1,11 @@
+"""
+Class family for Model IO classes to handle read/write of learning models
+"""
+
+class ModelIO(object):
+
+    def __init__(self):
+        pass
+
+    def write(self):
+        raise NotImplementedError()
