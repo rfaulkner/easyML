c39d4c6a716ce0fc3f913bb93c6e1c553e1e6fc9
rfaulk <rfaulk@yahoo-inc.com>
2013-12-04 23:36:50 -0800

    add - DataIOHDFS & build up interface.

--- versus/tools/dataIO.py
+++ versus/tools/dataIO.py
@@ -4,8 +4,24 @@ Class family for Data IO classes to handle data ingestion and fetch events
 
 class DataIO(object):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         pass
 
     def write(self):
         raise NotImplementedError()
+
+    def read(self):
+        raise NotImplementedError()
+
+
+class DataIOHDFS(DataIO):
+    """ Class for instrumenting data IO for HDFS. """
+
+    def __init__(self, **kwargs):
+        super(DataIOHDFS, self).__init__(**kwargs)
+
+    def write(self):
+        pass
+
+    def read(self):
+        pass
