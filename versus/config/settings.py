9819ddb0ac238edddf8735f5ed21c00a3dbe12b2
rfaulk <rfaulk@yahoo-inc.com>
2013-12-06 00:20:26 -0800

    add - project settings.

---
+++ versus/config/settings.py
@@ -0,0 +1,19 @@
+
+__flask_login_exists__ = True
+__secret_key__ = 'none'
+__version__ = '0.0.1'
+
+__instance_host__ = '127.0.0.1'
+__instance_port__ = 5000
+
+LICENSE = "BSD (Three Clause)"
+
+AUTHORS = {
+    'Ryan Faulkner': 'bobs.ur.uncle@gmail.com'
+}
+
+ADMINS = {
+    'Ryan Faulkner': 'bobs.ur.uncle@gmail.com'
+}
+
+USERS = {}
