c9afb40c0a664ab3664b3ddf437ebe42f48355be
rfaulk <rfaulk@yahoo-inc.com>
2013-12-07 16:38:07 -0800

    add - structure for tests.

---
+++ test/test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+    dynamic_learn.testsuite
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    :license: BSD, see LICENSE for more details.
+"""
+
+import unittest
+
+
+class TestLogistic(unittest.TestCase):
+    """ Test cases for GitDeploy initialization and config """
+    
+    def test_init(self):
+        assert False
+
+    def test_negative_log_likelihood(self):
+        assert False
