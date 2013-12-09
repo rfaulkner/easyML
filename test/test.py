82ca97df20921f68da34801c87be4702f4baafbf
rfaulk <rfaulk@yahoo-inc.com>
2013-12-08 18:39:45 -0800

    add - implementation for LogisticRegression unit tests.

--- test/test.py
+++ test/test.py
@@ -8,13 +8,40 @@
 """
 
 import unittest
+import theano.tensor as T
+
+from versus.src.logistic import LogisticRegression
+
+BATCH_SIZE = 50
 
 
 class TestLogistic(unittest.TestCase):
     """ Test cases for GitDeploy initialization and config """
-    
+
     def test_init(self):
-        assert False
+
+        # allocate symbolic variables for the data
+        x = T.fmatrix()  # the data is presented as rasterized images (each being a 1-D row vector in x)
+
+        # construct the logistic regression class
+        try:
+            LogisticRegression(_input=x.reshape((BATCH_SIZE , 28 * 28)), n_in=28 * 28, n_out=10)
+        except Exception:
+            assert False
+
+        assert True
 
     def test_negative_log_likelihood(self):
-        assert False
+
+        # allocate symbolic variables for the data
+        x = T.fmatrix()  # the data is presented as rasterized images (each being a 1-D row vector in x)
+        y = T.lvector()  # the labels are presented as 1D vector of [long int] labels
+
+        # construct the logistic regression class
+        try:
+            classifier = LogisticRegression(_input=x.reshape((BATCH_SIZE , 28 * 28)), n_in=28 * 28, n_out=10)
+            cost = classifier.negative_log_likelihood(y)
+        except Exception:
+            assert False
+
+        assert cost == 0
