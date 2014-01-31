0f4f058b391bf7937f772b31419395db582b03f4
rfaulk <rfaulk@yahoo-inc.com>
2014-01-30 23:11:43 -0800

    add/mod - redis io model / modify interface for DataIO.

--- versus/tools/dataIO.py
+++ versus/tools/dataIO.py
@@ -2,26 +2,42 @@
 Class family for Data IO classes to handle data ingestion and fetch events
 """
 
+import redis
+
+
 class DataIO(object):
 
     def __init__(self, **kwargs):
         pass
 
-    def write(self):
+    def write(self, **kwargs):
         raise NotImplementedError()
 
-    def read(self):
+    def read(self, **kwargs):
         raise NotImplementedError()
 
 
 class DataIOHDFS(DataIO):
-    """ Class for instrumenting data IO for HDFS. """
+    """ Class implementing data IO for HDFS. """
 
     def __init__(self, **kwargs):
         super(DataIOHDFS, self).__init__(**kwargs)
 
-    def write(self):
+    def write(self, **kwargs):
         pass
 
-    def read(self):
+    def read(self, **kwargs):
+        pass
+
+
+class DataIORedis(DataIO):
+    """ Class implementing data IO for Redis. """
+
+    def __init__(self, **kwargs):
+        super(DataIORedis, self).__init__(**kwargs)
+
+    def write(self, **kwargs):
+        pass
+
+    def read(self, **kwargs):
         pass
