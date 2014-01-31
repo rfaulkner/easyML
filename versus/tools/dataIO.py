06337736eb5894e4578e9db4776874c3e8bc03c9
rfaulk <rfaulk@yahoo-inc.com>
2014-01-30 23:37:57 -0800

    add - implementation for DataIORedis.

--- versus/tools/dataIO.py
+++ versus/tools/dataIO.py
@@ -3,6 +3,7 @@ Class family for Data IO classes to handle data ingestion and fetch events
 """
 
 import redis
+from versus.config import log
 
 
 class DataIO(object):
@@ -39,14 +40,43 @@ class DataIOHDFS(DataIO):
 class DataIORedis(DataIO):
     """ Class implementing data IO for Redis. """
 
+    DEFAULT_HOST = 'localhost'
+    DEFAULT_PORT = 6379
+    DEFAULT_DB = 0
+
     def __init__(self, **kwargs):
         super(DataIORedis, self).__init__(**kwargs)
 
+        self.conn = None
+
+        self.host = kwargs['host'] if kwargs.has_key('host') else \
+            self.DEFAULT_HOST
+        self.port = kwargs['port'] if kwargs.has_key('port') else \
+            self.DEFAULT_PORT
+        self.db = kwargs['db'] if kwargs.has_key('db') else self.DEFAULT_DB
+
     def connect(self, **kwargs):
-        pass
+        self.conn = redis.StrictRedis(host=self.host, port=self.port,
+                                      db=self.db)
 
     def write(self, **kwargs):
-        pass
+        if self.conn:
+            try:
+                return self.conn.set(kwargs['key'], kwargs['value'])
+            except KeyError as e:
+                log.error('Missing param -> {0}'.format(e.message))
+                return False
+        else:
+            log.error('No redis connection.')
+            return False
 
     def read(self, **kwargs):
-        pass
+        if self.conn:
+            try:
+                return self.conn.get(kwargs['key'])
+            except KeyError as e:
+                log.error('Missing param -> {0}'.format(e.message))
+                return False
+        else:
+            log.error('No redis connection.')
+            return False
