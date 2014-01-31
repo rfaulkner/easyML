5f49a8a4e91d1c1f2d29d876817f8ec314bcc9a3
rfaulk <rfaulk@yahoo-inc.com>
2014-01-31 00:21:06 -0800

    add - DataIORedis basic tests.

--- test/test.py
+++ test/test.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """
-    dynamic_learn.testsuite
-    ~~~~~~~~~~~~~~~~~~~~~~~
+dynamic_learn.testsuite
+~~~~~~~~~~~~~~~~~~~~~~~
 
-    :license: BSD, see LICENSE for more details.
+:license: BSD, see LICENSE for more details.
 """
 
 import unittest
 import theano.tensor as T
 
 from versus.src.logistic import LogisticRegression
+from versus.tools.dataIO import DataIORedis
 
 BATCH_SIZE = 50
 
@@ -51,7 +52,23 @@ class TestRedis(unittest.TestCase):
     """ Test cases for Redis read/writes """
 
     def test_init(self):
-        assert False
+        dior = DataIORedis()
+        dior.connect()  # connect to local instance
+
+        del dior
+        assert True
 
     def test_simple(self):
-        assert False
+        dior = DataIORedis()
+        dior.connect()  # connect to local instance
+
+        if not dior.write(key='try', value=1):
+            assert False
+
+        if not (dior.read(key='try') == 1):
+            assert False
+            
+        del dior
+        assert True
+
+
