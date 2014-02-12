1d7cac5e656ac605fbf7db3fb02db12dbe543b09
rfaulk <rfaulkner@wikimedia.org>
2014-02-11 23:48:54 -0800

    mod - more config for hadoop.

--- vagrant/setup_hadoop.sh
+++ vagrant/setup_hadoop.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
 # 1. Install Java
+
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install oracle-java7-installer
 
+
 # 2. Setup hadoop group & user
+
 sudo addgroup hadoop
 sudo adduser --ingroup hadoop hduser
 
+
 # 3. Setup hduser
+
 su - hduser
 ssh-keygen -t rsa -P ""
 cat .ssh/id_rsa.pub >> .ssh/authorized_keys
 
-# 4. Edit sudoers
-#
-# sudo visudo
-# hduser ALL=(ALL:ALL) ALL
 
 # 5. Disable IPv6
-#
-# sudo gedit /etc/sysctl.conf
-#
-# net.ipv6.conf.all.disable_ipv6 = 1
-# net.ipv6.conf.default.disable_ipv6 = 1
-# net.ipv6.conf.lo.disable_ipv6 = 1
-#
-# sudo sysctl -p
+
+sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+sudo sysctl -p
+
 
 # 6. Install Hadoop
 sudo add-apt-repository ppa:hadoop-ubuntu/stable
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install hadoop
+
+
+# 7. Modify .bashrc
+sudo cat bashrc_append.txt >> $HOME/.bashrc
