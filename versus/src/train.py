bab319e236fe0ea810ba5493763d0027b7ce3ff0
rfaulk <rfaulk@yahoo-inc.com>
2014-02-12 22:03:59 -0800

    add - module to handle model training.

---
+++ versus/src/train.py
@@ -0,0 +1,13 @@
+"""
+This module handles training models.
+
+    uses map reduce streaming jobs in python to train.
+    stores parameters in redis
+"""
+
+def train(labels, classifier):
+    """
+    :param labels:      Labels on which to train
+    :param classifier:  Classifier type
+    """
+    pass
