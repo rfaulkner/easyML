f0f4b2a41a497a56ac9f64c6dea90b1a1d8d610b
rfaulk <rfaulk@yahoo-inc.com>
2014-03-12 23:38:36 -0700

    mod - update schema to reflect schema defs

--- versus/schema/mysql.main.schema.sql
+++ versus/schema/mysql.main.schema.sql
@@ -1,7 +1,19 @@
 -- Main Model schema defined for MySQL
 
+CREATE TABLE `Users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `date_join` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `date_create` (`date_join`),
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `Models` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) unsigned NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
   `date_create` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `date_create` (`date_create`),
